4ebc0650c306df5376c657e297959a3523f92b6a
jbantin <jbantin@gmx.de>
2024-12-08 22:57:13 +0100

    v0.3

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const port = 2423;
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+app.get("/greeting", (req, res) => {
+  res.send("oi koile");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
